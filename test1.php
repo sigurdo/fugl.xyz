4c8278e962e6b37a939fed3686742e34cde450d5
sigurdht <legotull.99@gmail.com>
2020-01-16 09:10:07 +0100

    Create test1.php

---
+++ test1.php
@@ -0,0 +1,18 @@
+<?php
+//HTTPS redirect
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+	header('Location: '."https://folk.ntnu.no/sigurdht/fugl.xyz/test1.php");
+}
+if ($_POST) {
+
+}
+
+else {
+	$db = mysqli_connect("mysql.stud.ntnu.no", "sigurdht_test1", "litago", "sigurdht_fugl");
+	$result = mysqli_query($db, "SELECT * FROM test1");
+	//$row = mysqli_fetch_row($result);
+	while ($row = $result->fetch_assoc()) {
+		printf("Data: %s<br>", $row["data"]);
+	}
+}
+?>
