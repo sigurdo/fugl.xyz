60dea43f9d7105d05ae84f067c4b44d3eb282a91
sigurdo <36281346+sigurdo@users.noreply.github.com>
2020-02-06 09:42:04 +0100

    Fjerna passord for superuser

--- api/observations/index.php
+++ api/observations/index.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-	$db = mysqli_connect("mysql.stud.ntnu.no", "sigurdht_fugl", "litago", "sigurdht_fugl");
+	$db = mysqli_connect("mysql.stud.ntnu.no", "sigurdht_fugl-get", "password", "sigurdht_fugl");
 	$result = mysqli_query($db, "SELECT * FROM observation");
 	$array = array();
 	while ($row = mysqli_fetch_assoc($result)) {
