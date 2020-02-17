2346fc4f5cbbb70ef7fb04a6cd13d5cb16330f39
sigurdht <legotull.99@gmail.com>
2020-02-17 09:38:06 +0100

    Created api call to get turbine data

---
+++ api/turbines/index.php
@@ -0,0 +1,11 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $db = mysqli_connect("mysql.stud.ntnu.no", "sigurdht_fugl-get", "password", "sigurdht_fugl");
+    $result = mysqli_query($db, "SELECT * FROM turbine");
+    $array = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $array[] = $row;
+    }
+    echo json_encode($array);
+}
+?>
